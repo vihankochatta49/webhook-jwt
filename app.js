378aea5e737b78844f37c19446ba64d07fab645d
Vihan <vihankochatta49@gmail.com>
2022-11-02 21:53:04 +0530

    JWT

--- app.js
+++ app.js
@@ -30,34 +30,15 @@ app.post('/webhook', bodyParser.raw({ type: 'application/json' }), (req, res) =>
     }
     // Check to see if you received the event or not.
     console.log(event)
-    console.log(event.download_url);
+    console.log(event.payload.object.recording_files);
     if (req.headers.authorization === config.VERIFICATION_TOKEN) {
         res.status(200);
         res.send();
-        // var uuid = event.payload.object.uuid;
-        // //Double encode the uuid for validation incase it contains slashes
-        // var euuid = encodeURIComponent(encodeURIComponent(uuid));
-
-        // var options = {
-        //     uri: "https://api.zoom.us/v2/past_webinars/" + euuid + "/absentees",
-        //     auth: {
-        //         'bearer': token
-        //     },
-        //     headers: {
-        //         'User-Agent': 'Zoom-api-Jwt-Request',
-        //         'content-type': 'application/json'
-        //     },
-        //     json: true
-        // };
-
     } else {
-
         res.status(403).end('Access forbidden');
         console.log("Invalid Post Request.")
     }
 });
-
-
 
 app.listen(3000, () => {
     console.log('Server is up and running on port 3000.')
